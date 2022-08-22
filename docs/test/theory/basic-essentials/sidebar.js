7ec9f05f8b5f192b16eaac8cbd5ef7384ff01673
@xuefeng.ji <xuefvip@163.com>
2022-08-22 19:11:53 +0800

    docs: add README.md and 测试覆盖率.md

---
+++ docs/test/theory/basic-essentials/sidebar.js
@@ -0,0 +1,69 @@
+module.exports = [
+  {
+    title: '基础必备',
+	path: '/test/theory/basic-essentials',
+    collapsable: false,
+    children: [
+			{
+				title: '典型登录栗子讲解',
+				path: '/test/theory/basic-essentials/开篇之典型用户登录.md',
+				collapsable: true
+			},
+			{
+				title: '如何设计测试用例',
+				path: '/test/theory/basic-essentials/如何设计好的测试用例.md',
+				collapsable: true
+			},
+			{
+				title: '如何高效填写缺陷',
+				path: '/test/theory/basic-essentials/如何高效填写缺陷.md',
+				collapsable: true
+			},
+			{
+				title: '如何做好测试计划',
+				path: '/test/theory/basic-essentials/如何才能做好测试计划.md',
+				collapsable: true
+			},
+			{
+				title: '测试覆盖率',
+				path: '/test/theory/basic-essentials/测试覆盖率.md',
+				collapsable: true
+			},
+			{
+				title: '深入浅出静态方法',
+				path: '/test/theory/basic-essentials/深入浅出静态方法.md',
+				collapsable: true
+			},
+			{
+				title: '互联网产品如何设计测试策略',
+				path: '/test/theory/basic-essentials/互联网产品如何设计测试策略.md',
+				collapsable: true
+			},
+				{
+				title: '微服务下的 API 测试',
+				path: '/test/theory/basic-essentials/微服务下的API测试.md',
+				collapsable: true
+			},
+				{
+				title: '移动应用测试方法及思路',
+				path: '/test/theory/basic-essentials/移动应用测试方法及思路.md',
+				collapsable: true
+			},
+			{
+				title: '自动化理解及应用场景',
+				path: '/test/theory/basic-essentials/自动化理解及应用场景.md',
+				collapsable: true
+			},
+			{
+				title: '代码级测试方法',
+				path: '/test/theory/basic-essentials/代码级测试方法.md',
+				collapsable: true
+			},
+			{
+				title: '测试工程师核心竞争力',
+				path: '/test/theory/basic-essentials/测试工程师核心竞争力.md',
+				collapsable: true
+			}
+    ]
+  }
+]
