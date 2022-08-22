c30cdf2ef0f41b5f9cb8b6fccfc511fc96308770
@xuefeng.ji <xuefvip@163.com>
2022-08-22 20:45:28 +0800

    feat: add sidebar.js

---
+++ docs/test/theory/auto-test/sidebar.js
@@ -0,0 +1,51 @@
+module.exports = [
+  {
+    title: '自动化测试必备',
+		path: '/test/theory/auto-test/',
+    collapsable: false,
+    children: [
+			{
+			title: '提高 GUI 测试稳定性',
+			path: '/test/theory/auto-test/提高GUI测试稳定性',
+			collapsable: true
+			},
+			{
+			title: '大型项目 GUI 自动化测试策略',
+			path: '/test/theory/auto-test/大型项目GUI自动化测试设计策略',
+			collapsable: true
+			},
+			{
+				title: 'PO 设计模型',
+				path: '/test/theory/auto-test/PO-model',
+				collapsable: true
+			},
+			{
+				title: '数据驱动思想',
+				path: '/test/theory/auto-test/data-driven',
+				collapsable: true
+			},
+			{
+			title: '数据驱动之 yaml 文件',
+			path: '/test/theory/auto-test/yaml',
+			collapsable: true
+			},
+		   {
+			title: '关键字驱动设计',
+			path: '/test/theory/auto-test/keyword-driven-design',
+			collapsable: true
+		   },
+    ]
+  },
+	{
+		title: '自动化测试实战',
+		// path: '/test/theory/auto-test/',
+		collapsable: false,
+		children: [
+			{
+				title: 'Python+Excel+Json+Email 接口测试框架',
+				path: '/test/theory/auto-test/exercise/simple-frame',
+				collapsable: true
+			},
+		]
+	}
+]
