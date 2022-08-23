01c4fef2200a19f9618a54027bc5e77be1ce31cd
@xuefeng.ji <xuefvip@163.com>
2022-08-23 11:12:41 +0800

    feat: add sidebar.js

---
+++ docs/test/theory/interface-test/sidebar.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    title: '基础理论',
+	path: '/test/theory/interface-test/',
+    collapsable: false,
+    children: [
+    	{
+			title: 'Restful 规范',
+			path: '/test/theory/interface-test/Restful规范',
+			collapsable: true,
+		},
+		{
+			title: '三种状态机制介绍',
+			path: '/test/theory/interface-test/三种状态机制介绍',
+			collapsable: true,
+		},
+			{
+				title: 'Cookie 介绍',
+				path: '/test/theory/interface-test/cookie',
+				collapsable: true,
+			},
+			{
+				title: 'Session 介绍',
+				path: '/test/theory/interface-test/session',
+				collapsable: true,
+			},
+			{
+				title: 'Token 介绍',
+				path: '/test/theory/interface-test/token',
+				collapsable: true,
+			},
+			{
+				title: '跨域介绍',
+				path: '/test/theory/interface-test/跨域',
+				collapsable: true,
+			},
+    ]
+  }
+]
