48f8b74b6d9e396c9a3eaa18405b7c8882241f8b
@xuefeng.ji <xuefvip@163.com>
2022-08-24 10:20:51 +0800

    docs: add README.md

---
+++ docs/test/theory/performance-test/sidebar.js
@@ -0,0 +1,49 @@
+module.exports = [
+  {
+    title: '性能测试必备',
+	path: '/test/theory/performance-test/',
+    collapsable: false,
+    children: [
+			{
+				title: '性能测试概念',
+				path: '/test/theory/performance-test/性能测试概念.md',
+				collapsable: true
+			},
+			{
+				title: '性能基本指标',
+				path: '/test/theory/performance-test/性能基本指标.md',
+				collapsable: true
+			},
+			{
+				title: '指标理解',
+				path: '/test/theory/performance-test/指标理解.md',
+				collapsable: true
+			},
+			{
+				title: 'TPS与响应时间之间的关系',
+				path: '/test/theory/performance-test/TPS与响应时间之间的关系.md',
+				collapsable: true
+			},
+			{
+				title: '如何进行场景设计',
+				path: '/test/theory/performance-test/如何进行场景设计.md',
+				collapsable: true
+			},
+			{
+				title: '性能分析思路',
+				path: '/test/theory/performance-test/性能分析思路.md',
+				collapsable: true
+			},
+			{
+				title: '性能调优攻略',
+				path: '/test/theory/performance-test/性能调优攻略.md',
+				collapsable: true
+			},
+			{
+				title: '工具仅是工具',
+				path: '/test/theory/performance-test/工具仅是工具.md',
+				collapsable: true
+			}
+    ]
+  }
+]
