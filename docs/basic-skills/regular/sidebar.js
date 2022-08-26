50d48a11e084dc7f40bbd06ce300262edf5cdc05
@xuefeng.ji <xuefvip@163.com>
2022-08-26 11:28:08 +0800

    docs: add regular folder

---
+++ docs/basic-skills/regular/sidebar.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    title: '正则表达式',
+    collapsable: false,
+    children: [
+      {
+        title: '正则表达式之匹配普通字符语法',
+        path: '/basic-skills/regular/regular-character-syntax',
+        collapsable: true
+      },
+    ]
+  }
+]
