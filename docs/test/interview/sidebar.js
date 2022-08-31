6042f46e4abb2d76c93957c8550119d1a10a0b88
xuefengji <xuefvip@163.com>
2022-08-31 21:13:11 +0800

    docs: add interview folder

---
+++ docs/test/interview/sidebar.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    title: '面试相关',
+    collapsable: false,
+    children: [
+      {
+        title: '自动化面试001',
+        path: '/test/interview/自动化面试1.md'
+      },
+      {
+        title: '接口面试001',
+        path: '/test/interview/接口测试面经.md'
+      },
+      {
+        title: '基础面试001',
+        path: '/test/interview/面试题1.md'
+      },
+      {
+        title: '基础面试002',
+        path: '/test/interview/面试题2.md'
+      },
+      {
+        title: '编程题',
+        path: '/test/interview/编程题.md'
+      }
+    ]
+  },
+  {
+    title: '面试技巧',
+    collapsable: false,
+    children: [
+      {
+        title: '简历编写',
+        path: '/test/interview/面试技巧/简历编写和面试技巧.md'
+      },
+      {
+        title: '技能相关',
+        path: '/test/interview/面试技巧/技能相关.md'
+      },
+      {
+        title: '面试心理学',
+        path: '/test/interview/面试技巧/面试心理学.md'
+      }
+    ]
+  }
+]
