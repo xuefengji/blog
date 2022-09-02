d10eae235cbba95439859de3e9a877c04cc5a340
@xuefeng.ji <xuefvip@163.com>
2022-09-02 10:21:36 +0800

    docs: modify file name

--- docs/basic-skills/network/sidebar.js
+++ docs/basic-skills/network/sidebar.js
@@ -6,52 +6,52 @@ module.exports = [
     children: [
     		{
 				title: '网络分层',
-				path: '/basic-skills/network/网络分层.md',
+				path: '/basic-skills/network/network-layer.md',
 				collapsable: true
 			},
 			{
 				title: 'IP 地址',
-				path: '/basic-skills/network/IP地址.md',
+				path: '/basic-skills/network/ip-address.md',
 				collapsable: true
 			},
 			{
 				title: '子网掩码',
-				path: '/basic-skills/network/子网掩码.md',
+				path: '/basic-skills/network/subnet-mask.md',
 				collapsable: true
 			},
 			{
 				title: 'ARP 协议',
-				path: '/basic-skills/network/ARP协议.md',
+				path: '/basic-skills/network/arp-protocol.md',
 				collapsable: true
 			},
 			{
 				title: 'TCP 协议',
-				path: '/basic-skills/network/TCP协议.md',
+				path: '/basic-skills/network/tcp-protocol.md',
 				collapsable: true
 			},
 			{
 				title: 'UDP 协议',
-				path: '/basic-skills/network/UDP协议.md',
+				path: '/basic-skills/network/udp-protocol.md',
 				collapsable: true
 			},
 			{
 				title: 'HTTP 协议',
-				path: '/basic-skills/network/HTTP协议.md',
+				path: '/basic-skills/network/http-protocol.md',
 				collapsable: true
 			},
 			{
 				title: 'HTTPS 协议',
-				path: '/basic-skills/network/HTTPS.md',
+				path: '/basic-skills/network/https-protocol.md',
 				collapsable: true
 			},
 			{
 				title: 'DHCP 协议',
-				path: '/basic-skills/network/DHCP协议.md',
+				path: '/basic-skills/network/dhcp-protocol.md',
 				collapsable: true
 			},
 			{
 				title: 'DNS 协议',
-				path: '/basic-skills/network/DNS协议.md',
+				path: '/basic-skills/network/dns-protocol.md',
 				collapsable: true
 			}
 			
@@ -63,8 +63,8 @@ module.exports = [
 //    collapsable: false,
 //    children: [
 //    		{
-//				title: 'ARP 协议安全',
-//				path: '/basic-skills/network/协议安全/ARP协议安全.md',
+//				title: 'ARP ipsec-internet',
+//				path: '/basic-skills/network/ipsec-internet/ARPipsec-internet.md',
 //				collapsable: true
 //			}
 			
@@ -73,27 +73,27 @@ module.exports = [
 //},
 {
     title: '网络安全',
-	// path: '/basic-skills/network/协议安全',
+	// path: '/basic-skills/network/ipsec-internet',
     collapsable: false,
     children: [
    		{
 				title: 'ARP 攻击',
-				path: '/basic-skills/network/协议安全/ARP协议安全.md',
+				path: '/basic-skills/network/ipsec-internet/arp-ipsec.md',
 				collapsable: true
 			},
 			{
 				title: 'SYN Flood 攻击',
-				path: '/basic-skills/network/协议安全/SYN攻击.md',
+				path: '/basic-skills/network/ipsec-internet/syn-attack.md',
 				collapsable: true
 			},
 			{
 				title: 'ACK Flood 攻击',
-				path: '/basic-skills/network/协议安全/ack.md',
+				path: '/basic-skills/network/ipsec-internet/ack.md',
 				collapsable: true
 			},
 			{
 				title: 'DHCP Snooping',
-				path: '/basic-skills/network/协议安全/DHCP-Snooping.md',
+				path: '/basic-skills/network/ipsec-internet/dhcp-snooping.md',
 				collapsable: true
 			}
 
