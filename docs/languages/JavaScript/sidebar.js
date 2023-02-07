5d9292f9e68b1725bbc6770acb584ead3863346d
xuefengji <xuefvip@163.com>
2023-02-07 21:13:47 +0800

    refactor(language): add folder

---
+++ docs/languages/JavaScript/sidebar.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    title: 'JavaScript 基础',
+    path: '/languages/JavaScript/basics/',
+    collapsable: false,
+    children: [
+        {
+            title: 'JavaScript 变量',
+            path: '/languages/JavaScript/basics/variable',
+            collapsable: true
+        },
+    	
+    ]
+  },
+  
